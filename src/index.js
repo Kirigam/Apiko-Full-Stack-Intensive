71249e392f4214c2b991866e6b7e8cf3969176b1
Kirigam <hrytsiv.a.s@gmail.com>
2018-04-01 22:02:49 +0300

    Add task 4

--- src/index.js
+++ src/index.js
@@ -2,41 +2,96 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import data from './data';
+import styled from 'styled-components';
 
-let count = 10;
-let ls = data.slice(0,count);
-const buttonMore = document.getElementById('more');
-
-const PostCounter = ({count}) => <span>{count}</span>;
+let ls = data;
 
 const Posts = ({ list }) => (
-    <ol>
+    <ul>
       {list.map(item => (
-        <li key={item}>
+        <li key={item.id}>
           {item.title}
         </li>
       ))}
-    </ol>
+    </ul>
   );
 
+class PostsUsers extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 10
+        };
+
+       this.clickMore = this.clickMore.bind(this);
+    }
+    clickMore() {
+        let value = (this.state.count < 100)? this.state.count+10 : 100;
+        
+        this.setState({count: value});
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <h3>Users ask: {this.state.count}</h3>
+                <Posts list={ls.slice(0 ,this.state.count)} />
+                <button onClick={this.clickMore}>More</button>
+            </React.Fragment>
+        );
+    }
+}
+
 ReactDOM.render(
-    <Posts list={ls} />,
+    <PostsUsers />,
     document.getElementById('posts')
 );
 
-buttonMore.onclick = () => {
-    count = (count < 100)
-        ? count+10 : 100;
-    ls = data.slice(0,count);
-    
-    ReactDOM.render(
-        <PostCounter count={count} />,
-        document.getElementById('postCounter')
-    );
+/////////////////////////////////////////////////////////////////////
 
-    ReactDOM.render(
-        <Posts list={ls} />,
-        document.getElementById('posts')
-    );
+class SearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { search: '' };
+
+        this.hendleChange = this.hendleChange.bind(this);
+    }
+
+    hendleChange({target: {name, value}}) {
+        this.setState({[name]: value});
+        if(this.state[name] === '') {
+            ReactDOM.render(
+                <PostsUsers />,
+                document.getElementById('posts')
+            );
+        }else {
+            let pattern = new RegExp(value);
+         
+            let filtredArr = data.filter(item => {
+                if(item.title.match(pattern)) {
+                    return item;
+                }
+            });
+            
+            ls = filtredArr;
+            ReactDOM.render(
+               (filtredArr.length > 0)
+                ? <PostsUsers />
+                : <p>Not found</p>,
+
+                document.getElementById('posts')
+            );
+        } 
+    }
+
+    render() {
+        return (
+            <input type="text" name="search" 
+            placeholder="search" value={this.state.value} 
+            onChange={this.hendleChange} />
+        );
+    }
 }
 
+ReactDOM.render(
+    <SearchInput /> , document.getElementById('main')
+);
